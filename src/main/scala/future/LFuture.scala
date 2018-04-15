5a0d6245a005543591842a7497d97984d58d368c
Prashant Sable <prashant7090@gmail.com>
2018-04-15 14:58:08 +0530

    Added initial seed project

---
+++ src/main/scala/future/LFuture.scala
@@ -0,0 +1,14 @@
+package future
+
+/**
+  * Created by prashant on 15/4/18.
+  */
+object LFuture {
+
+  def main(args: Array[String]): Unit = {
+
+  }
+
+
+
+}
---
+++ src/test/scala/future/LFutureSuite.scala
@@ -0,0 +1,15 @@
+package future
+
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+
+/**
+  * Created by prashant on 15/4/18.
+  */
+@RunWith(classOf[JUnitRunner])
+class LFutureSuite extends FunSuite{
+  test("Test"){
+    assert(1===1)
+  }
+}
